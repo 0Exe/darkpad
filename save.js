36ab8d4b394dfb4f2f079758d3ebf67c90a7f53d
0Exe <69282787+0Exe@users.noreply.github.com>
2022-02-02 10:46:39 -0300

    note ids - different notes per id

--- save.js
+++ save.js
@@ -1,19 +1,23 @@
 async function load() {
     // load 
-    const notes = localStorage.getItem('notes');
-    console.log(notes)
+    const id = document.getElementById("id").value;
+    const notes = localStorage.getItem(id);
 
     document.getElementById('notes').value = notes;
 }
 
 load()
 
+document.getElementById('id').addEventListener('input', load)
+
 document.getElementById('notes').addEventListener('input', save);
 
 async function save() {
+    const id = document.getElementById("id").value;
+
     const notes = document.getElementById('notes').value;
 
     // save notes in local storage
     console.log(notes)
-    localStorage.setItem('notes', notes);
+    localStorage.setItem(id, notes);
 }
